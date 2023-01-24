6d61edf1646bf564939cc8451066cf88e17a8ecd
Murat Şahin <18155057@mersin.edu.tr>
2023-01-24 03:06:44 +0300

    Add project files.

---
+++ 18155057-GoruntuSifreleme/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace _18155057_GoruntuSifreleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DesifreEt = new System.Windows.Forms.Button();
+            this.KarmasikSifrele = new System.Windows.Forms.Button();
+            this.orijinal = new System.Windows.Forms.PictureBox();
+            this.hedef = new System.Windows.Forms.PictureBox();
+            this.EstetikSifreleme = new System.Windows.Forms.Button();
+            this.estetikDesifre = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.orijinal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hedef)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DesifreEt
+            // 
+            this.DesifreEt.Location = new System.Drawing.Point(343, 114);
+            this.DesifreEt.Name = "DesifreEt";
+            this.DesifreEt.Size = new System.Drawing.Size(128, 30);
+            this.DesifreEt.TabIndex = 0;
+            this.DesifreEt.Text = "Deşifre Et";
+            this.DesifreEt.UseVisualStyleBackColor = true;
+            this.DesifreEt.Click += new System.EventHandler(this.DesifreEt_Click);
+            // 
+            // KarmasikSifrele
+            // 
+            this.KarmasikSifrele.Location = new System.Drawing.Point(343, 78);
+            this.KarmasikSifrele.Name = "KarmasikSifrele";
+            this.KarmasikSifrele.Size = new System.Drawing.Size(128, 30);
+            this.KarmasikSifrele.TabIndex = 1;
+            this.KarmasikSifrele.Text = "Karmaşık Şifreleme";
+            this.KarmasikSifrele.UseVisualStyleBackColor = true;
+            this.KarmasikSifrele.Click += new System.EventHandler(this.KarmasikSifrele_Click);
+            // 
+            // orijinal
+            // 
+            this.orijinal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.orijinal.Location = new System.Drawing.Point(23, 12);
+            this.orijinal.Name = "orijinal";
+            this.orijinal.Size = new System.Drawing.Size(280, 426);
+            this.orijinal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orijinal.TabIndex = 2;
+            this.orijinal.TabStop = false;
+            this.orijinal.Click += new System.EventHandler(this.orijinal_Click);
+            // 
+            // hedef
+            // 
+            this.hedef.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hedef.Location = new System.Drawing.Point(509, 12);
+            this.hedef.Name = "hedef";
+            this.hedef.Size = new System.Drawing.Size(280, 426);
+            this.hedef.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hedef.TabIndex = 3;
+            this.hedef.TabStop = false;
+            // 
+            // EstetikSifreleme
+            // 
+            this.EstetikSifreleme.Location = new System.Drawing.Point(343, 199);
+            this.EstetikSifreleme.Name = "EstetikSifreleme";
+            this.EstetikSifreleme.Size = new System.Drawing.Size(128, 30);
+            this.EstetikSifreleme.TabIndex = 4;
+            this.EstetikSifreleme.Text = "Estetik Şifreleme";
+            this.EstetikSifreleme.UseVisualStyleBackColor = true;
+            this.EstetikSifreleme.Click += new System.EventHandler(this.EstetikSifreleme_Click);
+            // 
+            // estetikDesifre
+            // 
+            this.estetikDesifre.Location = new System.Drawing.Point(343, 235);
+            this.estetikDesifre.Name = "estetikDesifre";
+            this.estetikDesifre.Size = new System.Drawing.Size(128, 30);
+            this.estetikDesifre.TabIndex = 5;
+            this.estetikDesifre.Text = "Estetik Desifre";
+            this.estetikDesifre.UseVisualStyleBackColor = true;
+            this.estetikDesifre.Click += new System.EventHandler(this.estetikDesifre_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.estetikDesifre);
+            this.Controls.Add(this.EstetikSifreleme);
+            this.Controls.Add(this.hedef);
+            this.Controls.Add(this.orijinal);
+            this.Controls.Add(this.KarmasikSifrele);
+            this.Controls.Add(this.DesifreEt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.orijinal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hedef)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DesifreEt;
+        private System.Windows.Forms.Button KarmasikSifrele;
+        private System.Windows.Forms.PictureBox orijinal;
+        private System.Windows.Forms.PictureBox hedef;
+        private System.Windows.Forms.Button EstetikSifreleme;
+        private System.Windows.Forms.Button estetikDesifre;
+    }
+}
+
---
+++ 18155057-GoruntuSifreleme/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _18155057_GoruntuSifreleme
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap originalImage;
+        Bitmap destinationImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void orijinal_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosyaSecici = new OpenFileDialog();
+            dosyaSecici.Filter = "Image Files(*.JPG)|*.JPG;*.JPEG;*.jpeg;*.jpg";
+            DialogResult cevap = dosyaSecici.ShowDialog();
+            if (cevap == DialogResult.OK)
+            {
+                orijinal.Image = new Bitmap(dosyaSecici.FileName);
+                originalImage = (Bitmap)orijinal.Image;
+                resminBoyutunuAyarla();
+            }
+        }
+
+        private void resminBoyutunuAyarla()
+        {
+            Bitmap orijinalGoruntu = (Bitmap)orijinal.Image;
+            int genislik = orijinalGoruntu.Width;
+            int yukseklik = orijinalGoruntu.Height;
+            Bitmap hedefGoruntu;
+
+
+            if (((genislik % 2 == 0) && (yukseklik % 2 == 0)) || (((genislik % 2 == 1) && (yukseklik % 2 == 1))))
+            {
+                hedefGoruntu = new Bitmap(orijinalGoruntu, new Size(genislik + 1, yukseklik + 2));
+                orijinal.Image = hedefGoruntu;
+                destinationImage = hedefGoruntu;
+                Console.WriteLine("Hedefgoruntu genislik: " + hedefGoruntu.Width + ", yukseklik: " + hedefGoruntu.Height);
+                Console.WriteLine(destinationImage.Width + ", " + destinationImage.Height);
+            }
+            else
+            {
+                destinationImage = (Bitmap)orijinal.Image;
+            }
+        }
+
+        private void marshalRenkSifrele(Boolean isAesthetic)
+        {
+            if (orijinal.Image == null) return;
+
+            Bitmap orijinalGoruntu = destinationImage;
+            int genislik = orijinalGoruntu.Width;
+            int yukseklik = orijinalGoruntu.Height;
+
+            BitmapData orijinalGoruntuData = orijinalGoruntu.LockBits(new Rectangle(0, 0, genislik, yukseklik), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            int byteSayisi = orijinalGoruntuData.Stride * yukseklik;
+            byte[] orijinalPikseller = new byte[byteSayisi + 2];
+
+            Marshal.Copy(orijinalGoruntuData.Scan0, orijinalPikseller, 0, byteSayisi);
+
+            byte red, green, blue, newBluePiksel, newGreenPiksel, newRedPiksel;
+            for (int i = 0; i <= byteSayisi - 1; i = i + 3)
+            {
+                blue = orijinalPikseller[i];
+                green = orijinalPikseller[i + 1];
+                red = orijinalPikseller[i + 2];
+
+                if (isAesthetic == true)
+                {
+                    newBluePiksel = (byte)(blue - 536);
+                    newGreenPiksel = (byte)(green - 124);
+                    newRedPiksel = (byte)(red + 151);
+                }
+                else
+                {
+                    newBluePiksel = (byte)(blue - i);
+                    newGreenPiksel = (byte)(green - i);
+                    newRedPiksel = (byte)(red + i);
+                }
+
+                orijinalPikseller[i] = newBluePiksel;
+                orijinalPikseller[i + 1] = newGreenPiksel;
+                orijinalPikseller[i + 2] = newRedPiksel;
+
+            }
+            Bitmap hedefGoruntu = new Bitmap(genislik, yukseklik, PixelFormat.Format24bppRgb);
+
+            BitmapData hedefGoruntuData = hedefGoruntu.LockBits(new Rectangle(0, 0, genislik, yukseklik), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+            Marshal.Copy(orijinalPikseller, 0, hedefGoruntuData.Scan0, byteSayisi);
+            orijinalGoruntu.UnlockBits(orijinalGoruntuData);
+            hedefGoruntu.UnlockBits(hedefGoruntuData);
+            hedef.Image = hedefGoruntu;
+            destinationImage = hedefGoruntu;
+        }
+
+        private void marshalRenkCoz(Boolean isAesthetic)
+        {
+            if (orijinal.Image == null) return;
+
+            Bitmap orijinalGoruntu = destinationImage;
+            int genislik = orijinalGoruntu.Width;
+            int yukseklik = orijinalGoruntu.Height;
+
+            BitmapData orijinalGoruntuData = orijinalGoruntu.LockBits(new Rectangle(0, 0, genislik, yukseklik), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            int byteSayisi = orijinalGoruntuData.Stride * yukseklik;
+            byte[] orijinalPikseller = new byte[byteSayisi + 2];
+
+            Marshal.Copy(orijinalGoruntuData.Scan0, orijinalPikseller, 0, byteSayisi);
+
+            byte red, green, blue, newBluePiksel, newGreenPiksel, newRedPiksel;
+            for (int i = 0; i < byteSayisi - 1; i = i + 3)
+            {
+                blue = orijinalPikseller[i];
+                green = orijinalPikseller[i + 1];
+                red = orijinalPikseller[i + 2];
+
+                if (isAesthetic == true)
+                {
+                    newBluePiksel = (byte)(blue + 536);
+                    newGreenPiksel = (byte)(green + 124);
+                    newRedPiksel = (byte)(red - 151);
+                }
+                else
+                {
+                    newBluePiksel = (byte)(blue + i);
+                    newGreenPiksel = (byte)(green + i);
+                    newRedPiksel = (byte)(red - i);
+                }
+                
+
+                orijinalPikseller[i] = newBluePiksel;
+                orijinalPikseller[i + 1] = newGreenPiksel;
+                orijinalPikseller[i + 2] = newRedPiksel;
+
+            }
+            Bitmap hedefGoruntu = new Bitmap(genislik, yukseklik, PixelFormat.Format24bppRgb);
+
+            BitmapData hedefGoruntuData = hedefGoruntu.LockBits(new Rectangle(0, 0, genislik, yukseklik), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+            Marshal.Copy(orijinalPikseller, 0, hedefGoruntuData.Scan0, byteSayisi);
+            orijinalGoruntu.UnlockBits(orijinalGoruntuData);
+            hedefGoruntu.UnlockBits(hedefGoruntuData);
+            hedef.Image = hedefGoruntu;
+            destinationImage = hedefGoruntu;
+        }
+
+        //---------------------------SATIR VE SUTUNLARIN AYARLANDIĞI KISIM
+        private void sutunlariSagaSolaDagit()
+        {
+            if (orijinal.Image == null) return;
+
+            Bitmap orijinalGoruntu = destinationImage;
+            int genislik = orijinalGoruntu.Width;
+            int yukseklik = orijinalGoruntu.Height;
+            Bitmap hedefGoruntu = new Bitmap(genislik, yukseklik);
+
+            int evilI; Boolean flag = true;
+            for (int satir = 0; satir < genislik - 1; satir++)
+            {
+                for (int sutun = 0; sutun < yukseklik - 1; sutun++)
+                {
+                    Color currentPixel = orijinalGoruntu.GetPixel(satir, sutun);
+                    evilI = genislik - satir - 1;
+                    if (flag == true)
+                    {
+                        hedefGoruntu.SetPixel(evilI, sutun, currentPixel);
+                        flag = false;
+                    }
+                    else
+                    {
+                        hedefGoruntu.SetPixel(satir, sutun, currentPixel);
+                        flag = true;
+                    }
+                }
+            }
+            hedef.Image = hedefGoruntu;
+            destinationImage = hedefGoruntu;
+        }
+
+        private void satirlariAsagiYukariDagit()
+        {
+            if (orijinal.Image == null) return;
+
+            Bitmap orijinalGoruntu = destinationImage;
+            int genislik = orijinalGoruntu.Width;
+            int yukseklik = orijinalGoruntu.Height;
+            Bitmap hedefGoruntu = new Bitmap(genislik, yukseklik);
+
+            int evilI; Boolean flag = true;
+            for (int y = 0; y < yukseklik - 1; y++)
+            {
+                for (int x = 0; x < genislik - 1; x++)
+                {
+                    Color currentPixel = orijinalGoruntu.GetPixel(x, y);
+                    evilI = yukseklik - y - 1;
+                    if (flag == true)
+                    {
+                        hedefGoruntu.SetPixel(x, evilI, currentPixel);
+                        flag = false;
+                    }
+                    else
+                    {
+                        hedefGoruntu.SetPixel(x, y, currentPixel);
+                        flag = true;
+                    }
+                }
+            }
+            hedef.Image = hedefGoruntu;
+            destinationImage = hedefGoruntu;
+        }
+
+        private void sutunlariCoz()
+        {
+            if (orijinal.Image == null) return;
+
+            Bitmap orijinalGoruntu = destinationImage;
+            int genislik = orijinalGoruntu.Width;
+            int yukseklik = orijinalGoruntu.Height;
+            Bitmap hedefGoruntu = new Bitmap(genislik, yukseklik);
+            int evilI; Boolean flag = true;
+
+
+            for (int satir = 0; satir < genislik - 1; satir++)
+            {
+                for (int sutun = 0; sutun < yukseklik - 1; sutun++)
+                {
+                    Color currentPixel = orijinalGoruntu.GetPixel(genislik - satir - 1, sutun);
+                    evilI = genislik - satir - 1;
+                    if (flag == true)
+                    {
+                        hedefGoruntu.SetPixel(satir, sutun, currentPixel);
+                        flag = false;
+                    }
+                    else
+                    {
+                        hedefGoruntu.SetPixel(evilI, sutun, currentPixel);
+                        flag = true;
+                    }
+                }
+            }
+            hedef.Image = hedefGoruntu;
+            destinationImage = hedefGoruntu;
+            orijinal.Refresh();
+        }
+
+        private void satirlariCoz()
+        {
+            if (orijinal.Image == null) return;
+
+            Bitmap orijinalGoruntu = destinationImage;
+            int genislik = orijinalGoruntu.Width;
+            int yukseklik = orijinalGoruntu.Height;
+            Bitmap hedefGoruntu = new Bitmap(genislik, yukseklik);
+            int evilI; Boolean flag = true;
+
+
+            for (int y = 0; y < yukseklik - 1; y++)
+            {
+                for (int x = 0; x < genislik - 1; x++)
+                {
+                    Color currentPixel = orijinalGoruntu.GetPixel(x, yukseklik - y - 1);
+                    evilI = yukseklik - y - 1;
+                    if (flag == true)
+                    {
+                        hedefGoruntu.SetPixel(x, y, currentPixel);
+                        flag = false;
+                    }
+                    else
+                    {
+                        hedefGoruntu.SetPixel(x, evilI, currentPixel);
+                        flag = true;
+                    }
+                }
+            }
+            hedef.Image = hedefGoruntu;
+            destinationImage = hedefGoruntu;
+            orijinal.Refresh();
+        }
+
+        private void KarmasikSifrele_Click(object sender, EventArgs e)
+        {
+            sutunlariSagaSolaDagit();
+            satirlariAsagiYukariDagit();
+            marshalRenkSifrele(false);
+        }
+
+        private void DesifreEt_Click(object sender, EventArgs e)
+        {
+            marshalRenkCoz(false);
+            satirlariCoz();
+            sutunlariCoz();
+        }
+
+        private void EstetikSifreleme_Click(object sender, EventArgs e)
+        {
+            sutunlariSagaSolaDagit();
+            satirlariAsagiYukariDagit();
+            marshalRenkSifrele(true);
+        }
+
+        private void estetikDesifre_Click(object sender, EventArgs e)
+        {
+            marshalRenkCoz(true);
+            satirlariCoz();
+            sutunlariCoz();
+        }
+    }
+}
